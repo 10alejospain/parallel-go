780d8b82aea6bd1bcc1b5f4b826e8b73a935b0a3
10alejospain <10alejo.est@gmail.com>
2024-01-21 02:39:00 +0100

    concurrency with go routines

--- main.go
+++ main.go
@@ -2,16 +2,44 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
+func worker(inside_ptr *int, times int, routines int) {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	fmt.Print("hola")
+
+	for i := 0; i < (times / routines); i++ {
+		x := random.Float64()
+		y := random.Float64()
+
+		if x*x+y*y <= 1 {
+			*inside_ptr = *inside_ptr + 1
+		}
+	}
+}
+
 func main() {
-	var veces = flag.Int("n", 0, "Insert your name")
+	inside := 0
+	inside_ptr := &inside
+
+	var points = flag.Int("n", 0, "Number of points to generate")
+	var routines = flag.Int("r", 0, "Number of routines to use")
 	flag.Parse()
 
-	if *nFlag != "" {
-		log.Default().Println("Hello, " + *nFlag + "!")
-	} else {
-		log.Fatal("no name given")
+	// Time taken
+	startTime := time.Now()
+	defer func() {
+		endTime := time.Now()
+		elapsedTime := endTime.Sub(startTime)
+		fmt.Printf("Time taken: %v\n", elapsedTime)
+	}()
+
+	for i := 0; i < *routines; i++ {
+		go worker(inside_ptr, *points, *routines)
 	}
+	fmt.Printf("Aprox pi = %f, time = ", 4.0*float64(inside)/float64(*points))
 }
